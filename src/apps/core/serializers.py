a8c5a356d7b76ddbc9d286a57e6beaf5272e5d62
Benjie Jiao <hi@benjie.me>
2018-11-30 14:58:51 +0800

    Added inbound routes

--- src/apps/core/serializers.py
+++ src/apps/core/serializers.py
@@ -3,7 +3,9 @@ from apps.core.models import Airport
 
 
 class AirportSerializer(serializers.ModelSerializer):
+    sources = serializers.SerializerMethodField()
     destinations = serializers.SerializerMethodField()
+    location = serializers.SerializerMethodField()
 
     class Meta:
         fields = '__all__'
@@ -11,3 +13,12 @@ class AirportSerializer(serializers.ModelSerializer):
 
     def get_destinations(self, obj):
         return [x.dest.id if x.dest is not None else None for x in obj.routes_as_source.all()]
+
+    def get_sources(self, obj):
+        return [x.source.id if x.source is not None else None for x in obj.routes_as_dest.all()]
+
+    def get_location(self, obj):
+        if obj.location:
+            return obj.location.coords[:2]
+        else:
+            return None
--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -2,13 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from apps.core.models import Airport
 from apps.core.models import Route
+from django.db.models import Count
 
 from apps.core.serializers import AirportSerializer
 
 
 @api_view()
 def get_airports(request):
-    airports = Airport.objects.all()
+    airports = Airport.objects \
+        .annotate(source_count=Count('routes_as_source')) \
+        .annotate(dest_count=Count('routes_as_dest')) \
+        .filter(source_count__gt=0, dest_count__gt=0) \
+        .all()
 
     return Response({
         "data": AirportSerializer(airports, many=True).data
